99e32d2dfd681479fa00d69c4ef27e1a8e0f63b7
Otto Vehvilainen <otto.vehvilainen@iki.fi>
2017-04-25 08:16:09 +0300

    Initial Commit

---
+++ Link Opener/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Link Opener
+//
+//  Created by Otto Vehvilainen on 25/04/2017.
+//  Copyright © 2017 Otto Vehvilainen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
